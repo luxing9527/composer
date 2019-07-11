89738e694309231296e57034eb4628e72a9a9a6d
luxing <1151368955@qq.com>
2019-07-11 16:51:10 +0800

    first commit

---
+++ test/ComposerTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luxing
+ * Date: 2019/7/11
+ * Time: 14:54
+ */
+
+class ComposerTest
+{
+    public function __construct()
+    {
+    }
+
+    public static function test(){
+        return 'Hello World!';
+    }
+}
